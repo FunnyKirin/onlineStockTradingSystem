f3cd59f91720a155813ecab0fb5ed462e1a05d67
Jie Liang <jieliang456@gmail.com>
2016-03-23 02:25:33 -0400

    Update 3point3.sql

--- 3point3.sql
+++ 3point3.sql
@@ -45,3 +45,25 @@ CREATE PROCEDURE personalizedStocks(IN CustID INTEGER)
 BEGIN
 	CALL giveSuggestion(CustID);
 END//
+
+# Stocks available of a particular type and most-recent order info
+DELIMITER //
+CREATE PROCEDURE specificStockWithRecentInfo(IN PriceType CHAR(20))
+ BEGIN
+    SELECT StockOrder.*
+	FROM Stock, StockOrder
+	WHERE StockOrder.PriceType = PriceType AND StockOrder.PriceType = PriceType AND (
+		SELECT MAX(StockOrder.DateTime)
+		FROM StockOrder);
+ END //
+
+# Best-Seller list of stocks
+DELIMITER //
+CREATE PROCEDURE bestSellerStocks()
+ BEGIN
+    SELECT Trade.StockId
+	FROM Trade, StockOrder
+	WHERE Trade.OrderId = StockOrder.ID AND StockOrder.OrderType = 'sell'
+	GROUP BY Trade.StockId
+	ORDER BY COUNT(Trade.StockId) DESC;
+ END //
