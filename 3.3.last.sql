0ffc760b175e96206051bfe6a3c4f00745506ed5
Jie Liang <jieliang456@gmail.com>
2016-03-23 00:01:40 -0400

    Stocks available of a particular type and most-recent order info

--- 3.3.last.sql
+++ 3.3.last.sql
@@ -1,10 +1,12 @@
-#Stocks avaliable of a particular type and most-recent order info
+# Stocks available of a particular type and most-recent order info
 DELIMITER //
-CREATE PROCEDURE specificStockWithRecentInfo()
+CREATE PROCEDURE specificStockWithRecentInfo(IN PriceType CHAR(20))
  BEGIN
-    SELECT Trade.StockId
-	FROM Trade, StockOrder
-	WHERE Trade.OrderId = StockOrder.ID
+    SELECT StockOrder.*
+	FROM Stock, StockOrder
+	WHERE StockOrder.PriceType = PriceType AND StockOrder.PriceType = PriceType AND (
+		SELECT MAX(StockOrder.DateTime)
+		FROM StockOrder)
  END //
 DELIMITER;
 
