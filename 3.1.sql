81e64af63b1fb64fa3a64c090e75fdc7592527a6
Jie Liang <jieliang456@gmail.com>
2016-03-23 00:23:19 -0400

    merge 3.1 and 3.3

--- 3.1.sql
+++ 3.1.sql
@@ -220,3 +220,27 @@ CREATE PROCEDURE activeStocks()
 	ORDER BY COUNT(Trade.StockId) DESC
  END //
 DELIMITER;
+
+# Stocks available of a particular type and most-recent order info
+DELIMITER //
+CREATE PROCEDURE specificStockWithRecentInfo(IN PriceType CHAR(20))
+ BEGIN
+    SELECT StockOrder.*
+	FROM Stock, StockOrder
+	WHERE StockOrder.PriceType = PriceType AND StockOrder.PriceType = PriceType AND (
+		SELECT MAX(StockOrder.DateTime)
+		FROM StockOrder)
+ END //
+DELIMITER;
+
+# Best-Seller list of stocks
+DELIMITER //
+CREATE PROCEDURE bestSellerStocks()
+ BEGIN
+    SELECT Trade.StockId
+	FROM Trade, StockOrder
+	WHERE Trade.OrderId = StockOrder.ID AND StockOrder.OrderType = 'sell'
+	GROUP BY Trade.StockId
+	ORDER BY COUNT(Trade.StockId) DESC
+ END //
+DELIMITER;
