9f86abd934b8bdb0455db7fc05983e58639fe834
Hey <Human-Outfit@users.noreply.github.com>
2016-03-23 00:41:37 -0400

    Update 3point3.sql

--- 3point3.sql
+++ 3point3.sql
@@ -1,43 +1,51 @@
+# A customer's current stock holdings 
 DELIMITER //
 CREATE PROCEDURE listStock(IN CustID INTEGER)
 BEGIN
 	SELECT S.*, NumShares
     FROM Stock S, Account A, hasStcok H
     WHERE A.ClientID = CustID;
-END;
+END//
 DELIMITER;
 
+# A history of all current and past orders a customer has placed 
 DELIMITER //
-CREATE PROCEDURE orderHistory()
+CREATE PROCEDURE orderHistory(CustID INTEGER)
 BEGIN
-	SELECT *
-    FROM StockOrder;
-END;
+	SELECT O.ID, S.StockSymbol, O.NumShares, O.PriceType, O.OrderType 
+    FROM StockOrder O, Trade T, Stock S, Account A
+    WHERE A.ClientID = CustID AND A.ID = T.AccountId 
+		AND T.OrderId = O.ID AND S.StockSymbol = T.StockId;
+END//
 DELIMITER;
 
-DELIMITER //
-CREATE PROCEDURE orderHistory()
-BEGIN
-	SELECT *
-    FROM StockOrder;
-END;
-DELIMITER;
-
+# Stocks available with a particular keyword or set of keywords in the stock name, and most-recent order info 
 DELIMITER //
 CREATE PROCEDURE searchAvailStockByName(IN SName CHAR(20))
 BEGIN
-	SELECT *
-    FROM Stock S
+	SELECT DISTINCT *
+    FROM Stock S, StockOrder O
     WHERE CompanyName LIKE CONCAT('%', CONCAT(SNAME, '%'))
-		AND NumShares > 0; 
-END;
+		AND O.StockSymbol = S.StockSymbol
+	GROUP BY S.StockSymbol;
+END//
 DELIMITER;
 
+# Stocks available of a particular type and most-recent order info 
 DELIMITER //
 CREATE PROCEDURE searchAvailStockByType(IN SType CHAR(20))
 BEGIN
 	SELECT *
-    FROM Stock S
-    WHERE CompanyName LIKE CONCAT('%', CONCAT(SType, '%'))
-		AND NumShares > 0;
-END;
+    FROM Stock S, StockOrder O
+    WHERE S.Type LIKE CONCAT('%', CONCAT(SType, '%'))
+		AND O.StockSymbol = S.StockSymbol
+	GROUP BY S.StockSymbol;
+END//
+DELIMITER;
+
+# Personalized stock suggestion list 
+DELIMITER //
+CREATE PROCEDURE personalizedStocks(IN CustID INTEGER)
+BEGIN
+	CALL giveSuggestion(CustID);
+END//
