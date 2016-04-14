518d8030af4c9e84723ff82c6ae46627ead60977
Jie Liang <jieliang456@gmail.com>
2016-04-14 10:16:05 -0400

    set up index.html and basic login.html

---
+++ WebContent/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+         $window = $(window);
+         $("#fixmenu").addClass("active");
+    // $(window).scroll(function(){
+
+    //     if($window.scrollTop() > 100)
+    //         $("#fixmenu").addClass("active");
+    //     else    
+    //         $("#fixmenu").removeClass("active");
+    // });
+});
