b3abb2a07b5f3d6b750c9bbe1b36765bab5f792e
Jie Liang <jieliang456@gmail.com>
2016-04-16 12:26:59 -0400

    create basic structure

---
+++ src/webapp/beans/UserAccount.java
@@ -0,0 +1,28 @@
+package webapp.beans;
+
+public class UserAccount {
+	   private String userName;
+	   private String gender;
+	   private String password;
+	    
+	 
+	   public UserAccount() {
+	        
+	   }
+	    
+	   public String getUserName() {
+	       return userName;
+	   }
+	 
+	   public void setUserName(String userName) {
+	       this.userName = userName;
+	   }
+	 
+	   public String getPassword() {
+	       return password;
+	   }
+	 
+	   public void setPassword(String password) {
+	       this.password = password;
+	   }
+}
