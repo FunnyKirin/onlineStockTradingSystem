768e076bf89ee6da9f4b4b51651ecb2f75d3c035
Zhengyang Liu <liuzhengyang183@gmail.com>
2016-05-05 10:04:39 -0400

    no message

--- mysql/transaction-update.sql
+++ mysql/transaction-update.sql
@@ -50,5 +50,28 @@ INSERT INTO Trade(AccountId, BrokerId, OrderId, StockId, TransactionId)
 VALUES(1, 1, 1, 'GM', 1),
 		(2, 2, 2, 'IBM', 2);
         
-
+INSERT INTO stockHistory(StockSymbol, PricePerShare, stockDate)
+VALUES	('F', 1, '2016-05-01'),
+		('F', 1.5, '2016-05-02'),
+        ('F', 1.2, '2016-05-03'),
+        ('F', 2, '2016-05-04');
+        
+INSERT INTO stockHistory(StockSymbol, PricePerShare, stockDate)
+VALUES	('GM', 1, '2016-05-01'),
+		('GM', 1.5, '2016-05-02'),
+        ('GM', 1.2, '2016-05-03'),
+        ('GM', 2, '2016-05-04');
+        
+INSERT INTO stockHistory(StockSymbol, PricePerShare, stockDate)
+VALUES	('IBM', 1, '2016-05-01'),
+		('IBM', 1.5, '2016-05-02'),
+        ('IBM', 1.2, '2016-05-03'),
+        ('IBM', 2, '2016-05-04');
+        
+        INSERT INTO stockHistory(StockSymbol, PricePerShare, stockDate)
+VALUES	('F', 1, '2015-05-01'),
+		('F', 1.5, '2015-05-02'),
+        ('F', 1.2, '2015-05-03'),
+        ('F', 2, '2015-05-04');
+        
 commit;
